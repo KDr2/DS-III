a1a9b52b2b72583f67700cca223da48d73a28517
KDr2 <zhuo@hexoasis.com>
2016-06-17 22:34:29 +0800

    sep with empty p, get data using mojo ua

--- castles/ds3-web/Tweet.pm
+++ castles/ds3-web/Tweet.pm
@@ -7,12 +7,21 @@ use warnings;
 use Encode;
 use LWP::UserAgent;
 use JSON;
-use DateTime;
 use Data::Dumper;
 use HTML::TreeBuilder;
+use Mojo::UserAgent;
+
 use WebConfig;
 
-sub fetch_data {
+sub fetch_data_mojo {
+    state $host = 'https://platform.quip.com/1/threads/';
+    my ($thread_id) = @_;
+    my $ua = Mojo::UserAgent->new;
+    my $data = $ua->get($host . $thread_id => {"Authorization" => $quip_auth_key})->res->body;
+    return $data;
+}
+
+sub fetch_data_lwp {
     state $host = 'https://platform.quip.com/1/threads/';
     my ($thread_id) = @_;
     my $user_agent = LWP::UserAgent->new("Mozilla Firefox");
@@ -22,6 +31,14 @@ sub fetch_data {
     my $response = $user_agent->get($host . $thread_id);
     my $data = $response->decoded_content;
     return $data;
+}
+
+sub fetch_data {
+    my ($thread_id) = @_;
+    if (defined($ENV{GATEWAY_INTERFACE}) && $ENV{GATEWAY_INTERFACE} =~ /^CGI\/.*/) {
+        return fetch_data_lwp($thread_id);
+    }
+    return fetch_data_mojo($thread_id);
 }
 
 sub get_html {
@@ -40,8 +57,10 @@ sub strip_tags {
 
 sub html2json {
     my $text = shift;
-    my @tweets = split(qr|<hr/?>|, $text);
-    @tweets = map { strip_tags($_) } @tweets;
+    my @tweets = split(qr#<hr/?>|<p.*?>.{0,1}</p>#, $text);
+    @tweets = grep {!/^\s*$/} @tweets;
+    @tweets = map { s/^\s+|\s+$//g; $_ } @tweets;
+    # @tweets = map { strip_tags($_) } @tweets;
     shift @tweets;
     return @tweets;
 }
