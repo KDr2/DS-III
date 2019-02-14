612fe576d585dea5905dd269237e4b72030560c9
KDr2 <zhuo@hexoasis.com>
2019-02-14 20:45:03 +0800

    script updates

--- castles/cxan/scripts/pkg.inc.tmpl.php
+++ castles/cxan/scripts/pkg.inc.tmpl.php
@@ -1,6 +1,6 @@
 <?php
 
-define(WEBSITE_ROOT, "PATH_TO_WEBSITE_ROOT");
-define(UPLOAD_TOKEN, "a secure token");
+define("WEBSITE_ROOT", "PATH_TO_WEBSITE_ROOT");
+define("UPLOAD_TOKEN", "a secure token");
 
 $_REQUEST['token'] != UPLOAD_TOKEN and die("error");
--- scripts/skia/build.sh
+++ scripts/skia/build.sh
@@ -20,7 +20,16 @@ skia_use_system_icu=false
 skia_use_system_libjpeg_turbo=false
 skia_use_system_libpng=false
 skia_use_system_libwebp=false
+extra_cflags=["-static-libstdc++", "-static-libgcc"]
 EOF
 
 
 bin/gn gen out/Shared --args='is_official_build=true is_component_build=true'
+
+cat > /dev/null <<EOF
+# run test, see https://skia.org/dev/testing/testing
+# python tools/git-sync-deps
+bin/gn gen out/Debug
+ninja -C out/Debug dm
+out/Debug/dm -v -w dm_output
+EOF
