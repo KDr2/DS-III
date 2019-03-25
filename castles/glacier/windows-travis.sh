92f549d0022799ed162c927e794c6a73d982bf38
KDr2 <zhuo@hexoasis.com>
2019-03-25 14:15:39 +0800

    pry windowsn env on travis-ci

---
+++ castles/glacier/windows-travis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ex
+
+curl --version
+# choco install julia
+# Julia installed to 'C:\Users\travis\AppData\Local\Julia-1.1.0'
+# Added C:\ProgramData\chocolatey\bin\julia.exe shim pointed to 'c:\users\travis\appdata\local\julia-1.1.0\bin\julia.exe'.
+echo =======================
+echo $PWD
+echo =======================
