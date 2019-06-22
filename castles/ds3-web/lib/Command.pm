96d462c34a7d9b765f7e633cbd17bc34dbe50594
KDr2 <zhuo@hexoasis.com>
2019-06-22 20:53:26 +0800

    commands for GCP

--- castles/ds3-web/lib/Command.pm
+++ castles/ds3-web/lib/Command.pm
@@ -5,9 +5,30 @@ use strict;
 use warnings;
 
 my %COMMANDS = (
-    Default => "free",
-    Dir => "ls",
+    Default => "uptime",
+    Memory => "free -m",
     GCP_List_Instances => "gcloud compute instances list",
+    GCP_List_Machine_Types => "gcloud compute machine-types list",
+    GCP_Stop_Instance1 =>
+    "gcloud compute instances stop instance-1 --zone=asia-east1-b",
+    GCP_Boot_Instance1 =>
+    "gcloud compute instances start instance-1 --zone=asia-east1-b",
+    GCP_Resize_Instance1_f1_micro =>
+    "gcloud compute instances set-machine-type instance-1 --machine-type=f1-micro --zone=asia-east1-b",
+    GCP_Resize_Instance1_g1_small =>
+    "gcloud compute instances set-machine-type instance-1 --machine-type=g1-small --zone=asia-east1-b",
+    GCP_Resize_Instance1_n1_highcpu2 =>
+    "gcloud compute instances set-machine-type instance-1 --machine-type=n1-highcpu-2 --zone=asia-east1-b",
+    GCP_Resize_Instance1_n1_highcpu4 =>
+    "gcloud compute instances set-machine-type instance-1 --machine-type=n1-highcpu-4 --zone=asia-east1-b",
+    GCP_Resize_Instance1_n1_highmem2 =>
+    "gcloud compute instances set-machine-type instance-1 --machine-type=n1-highmem-2 --zone=asia-east1-b",
+    GCP_Resize_Instance1_n1_highmem4 =>
+    "gcloud compute instances set-machine-type instance-1 --machine-type=n1-highmem-4 --zone=asia-east1-b",
+    GCP_Stop_Instance2 =>
+    "gcloud compute instances stop instance-2 --zone=us-central1-a",
+    GCP_Boot_Instance2 =>
+    "gcloud compute instances start instance-2 --zone=us-central1-a",
 );
 
 
@@ -27,7 +48,9 @@ sub run_command {
       commands => {%COMMANDS},
       cmd_label => $cmd,
       command => $COMMANDS{$cmd},
-      stdout => $output);
+      return_code => $?,
+      stdout => $output,
+  );
   $c->render("/board-command");
 }
 
