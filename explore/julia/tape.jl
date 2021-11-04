49508ab939264ce81098823094442ac2dc7b653c
KDr2 <zhuo@hexoasis.com>
2021-11-04 07:22:58 +0000

    taped function

--- explore/julia/tape.jl
+++ explore/julia/tape.jl
@@ -1,5 +1,5 @@
 using IRTools
-# using MacroTools
+using MacroTools
 
 mutable struct Instruction{F}
     fun::F
@@ -20,7 +20,6 @@ box(x, t::Tape) = Box(x, t)
 
 tape(x) = nothing
 tape(x::Box) = x.tape
-tape(x::Tape) = x
 function tape(x::Tuple)
     for i in x
         tape(i) != nothing && return tape(i)
@@ -33,25 +32,28 @@ function Base.show(io::IO, instruction::Instruction)
 end
 
 function Base.show(io::IO, tp::Tape)
-    println("$(length(tp))-element Tape:")
+    buf = IOBuffer()
+    print(buf, "$(length(tp))-element Tape")
+    isempty(tp) || println(buf, ":")
     i = 1
     for instruction in tp
-        print(io, "$i => ")
-        show(io, instruction)
+        print(buf, "\t$i => ")
+        show(buf, instruction)
         i += 1
     end
+    print(io, String(take!(buf)))
 end
 
-function (ins::Instruction{F})() where F
-    output = ins.fun(map(val, ins.input)...)
-    ins.output.val = output
+function (instr::Instruction{F})() where F
+    output = instr.fun(map(val, instr.input)...)
+    instr.output.val = output
 end
 
 function run(tape::Tape, args...)
     input = map(x-> box(x, tape), args)
     tape[1].input = input
-    for ins in tape
-        ins()
+    for instruction in tape
+        instruction()
     end
 end
 
@@ -67,15 +69,22 @@ end
 function intercept(ir)
     ir == nothing && return
     tape = pushfirst!(ir, IRTools.xcall(@__MODULE__, :Tape))
+
+    # here we assumed the ir only has a return statement at its last block,
+    # and we make sure the return value is from a function call (to `identity`)
+    last_blk = IRTools.blocks(ir)[end]
+    retv = IRTools.returnvalue(last_blk)
+    IRTools.return!(last_blk, IRTools.xcall(Base, :identity, retv))
+
     for (x, st) in ir
         x == tape && continue
         Meta.isexpr(st.expr, :call) || continue
         ir[x] = IRTools.xcall(@__MODULE__, :run_and_record!, tape, st.expr.args...)
     end
+    # the real return value will be in the last instruction on the tape
     IRTools.return!(ir, tape)
     return ir
 end
-
 
 
 IRTools.@dynamo function record(a...)
@@ -84,28 +93,66 @@ IRTools.@dynamo function record(a...)
 end
 
 
+mutable struct TapedFunction
+    func::Function
+    ir::Union{Nothing, IRTools.IR}
+    tape::Tape
+    cur_idx::Int
+    TapedFunction(f) = new(f, nothing, Instruction[], 0)
+end
+
+function (tf::TapedFunction)(args...)
+    if isempty(tf.tape)
+        ir = IRTools.@code_ir tf.func(args...)
+        ir = intercept(ir)
+        tape = IRTools.evalir(ir, tf.func, args...)
+        tf.ir = ir
+        tf.tape = tape
+        return result(tape)
+    else
+        run(tf.tape, args...)
+        return result(tf.tape)
+    end
+end
+
+function Base.show(io::IO, tf::TapedFunction)
+    buf = IOBuffer()
+    println(buf, "TapedFunction:")
+    println(buf, "  .func => $(tf.func)")
+    println(buf, "  .ir   => $(tf.ir)")
+    println(buf, "  .tape => $(tf.tape)")
+    print(io, String(take!(buf)))
+end
+
+macro tapedfunction(expr)
+    d = MacroTools.splitdef(expr)
+    f = esc(d[:name])
+    new_f_name = gensym(d[:name])
+    d[:name] = new_f_name
+    origin_func = MacroTools.combinedef(d)
+
+    return quote
+        $origin_func
+        $f = TapedFunction($new_f_name)
+    end
+end
+
 ### Testing
 
 f1(x) = x + 1
-f2(x) = x * 2
-f3(x) = x ^ 3
+f2(x) = x * 2, x + 2
+f3(x) = x * 3
 
-# debug
-#=
-f(x) = f1(x)
-ir = @code_ir f(2)
-ir2 = intercept(ir)
-@show ir
-@show ir2, IRTools.evalir(ir2, f, 2)
-=#
-
-tp = record() do
-    a1 = f1(2)
-    a2 = f2(a1)
-    a3 = f3(a2)
+@tapedfunction function t1(x)
+    a1 = f1(x)
+    a2, a3 = f2(a1)
+    a2
 end
 
-@show tp
-println("tp(2) = ", result(tp))
-run(tp, 20)
-println("tp(20) = ", result(tp))
+@show t1
+println()
+@show t1(2)
+println()
+@show t1
+println()
+@show t1(3)
