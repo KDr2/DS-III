da47aa4736a55030fa4900917587e6118e8a244e
KDr2 <zhuo@hexoasis.com>
2021-11-30 02:23:34 +0000

    update pdfium build script

--- scripts/pdfium/build.sh
+++ scripts/pdfium/build.sh
@@ -2,33 +2,88 @@
 
 cat > /dev/null <<EOF
 options:
+- -b|--branch: branch to build
 - --shared: build shared lib
 - --static: build static lib
 - --skia: build with skia
+- -s|--sync: sync before building
 - --debug: build in DEBUG mode
-- -s: sync before building
 EOF
 
-set -ex
+# set -ex
 
+### OPTIONS
+
+OPT_BRANCH=-
+OPT_SHARED=YES
+OPT_STATIC=NO
+OPT_SKIA=NO
+OPT_CHECKOUT=NO
+OPT_SYNC=NO
+OPT_DEBUG=NO
+
+POSITIONAL_ARGS=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -b|--branch)
+            OPT_BRANCH="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        --share|--shared)
+            OPT_SHARED=YES
+            OPT_STATIC=NO
+            shift # past argument
+            ;;
+        --static)
+            OPT_SHARED=NO
+            OPT_STATIC=YES
+            # SEARCHPATH="$2"
+            shift # past argument
+            ;;
+        --skia)
+            OPT_SKIA=YES
+            shift # past argument
+            ;;
+        --checkout)
+            OPT_CHECKOUT=YES
+            shift # past argument
+            ;;
+        -s|--sync)
+            OPT_SYNC=YES
+            shift # past argument
+            ;;
+        --debug)
+            OPT_DEBUG=YES
+            shift # past argument
+            ;;
+        *)  # unknown option
+            POSITIONAL_ARGS+=("$1") # save it in an array for later
+            echo "Unknow option: $1"
+            shift # past argument
+            ;;
+    esac
+done
+
+## OS name
 OS=$(uname)
 case $OS in
-MINGW*)
-  OS="windows"
-  ;;
-*)
-  OS=$(echo $OS | tr '[:upper:]' '[:lower:]')
-  ;;
+    MINGW*)
+        OS="windows"
+        ;;
+    *)
+        OS=$(echo $OS | tr '[:upper:]' '[:lower:]')
+        ;;
 esac
 
 # Input
-PDFium_REPO='https://github.com/PaodingAI/pdfium.git'
-PDFium_SOURCE_DIR=$PWD
-PDFium_SOURCE_REVISON=$(git rev-parse --short HEAD)
-PDFium_SOURCE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-PDFium_BUILD_DIR="$PDFium_SOURCE_DIR/out/$PDFium_SOURCE_BRANCH"
+PDFIUM_REPO='https://github.com/PaodingAI/pdfium.git'
+PDFIUM_SOURCE_DIR=$PWD
+PDFIUM_SOURCE_REVISON=$(git rev-parse --short HEAD)
+PDFIUM_SOURCE_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 BUILD_MODE='RELEASE'
-if [[ $(getopt -q -l debug -- $@) = *--debug* ]]; then
+if [[ $OPT_DEBUG == YES ]]; then
     BUILD_MODE='DEBUG'
 fi
 
@@ -48,27 +103,37 @@ solutions = [
 EOF
 
 # make the repo clean before sync
-git -C $PDFium_SOURCE_DIR/ checkout BUILD.gn
-[[ -d $PDFium_SOURCE_DIR/build ]] && git -C $PDFium_SOURCE_DIR/build checkout .
+if [[ $OPT_CHECKOUT == YES ]]; then
+    git -C $PDFIUM_SOURCE_DIR/ checkout BUILD.gn
+    [[ -d $PDFIUM_SOURCE_DIR/build ]] && git -C $PDFIUM_SOURCE_DIR/build checkout .
+fi
 
-if [[ $(getopt -q -o s -- $@) == *-s* ]]; then
+if [[ $OPT_BRANCH != - ]]; then
+    PDFIUM_SOURCE_BRANCH=$OPT_BRANCH
+    git checkout $OPT_BRANCH
+fi
+PDFIUM_BUILD_DIR="$PDFIUM_SOURCE_DIR/out/$PDFIUM_SOURCE_BRANCH"
+echo building branch [$PDFIUM_SOURCE_BRANCH]...
+
+
+if [[ $OPT_SYNC == YES ]]; then
     gclient sync --gclientfile=pdfium.gclient
 fi
 
 # set visibility to default
 perl -p -i.bak \
      -e 's#//build/config/gcc:symbol_visibility_hidden#//build/config/gcc:symbol_visibility_default#gi' \
-     $PDFium_SOURCE_DIR/build/config/BUILDCONFIG.gn
+     $PDFIUM_SOURCE_DIR/build/config/BUILDCONFIG.gn
 
-if [[ $(getopt -q -l shared -o n -- $@) == *--shared* ]]; then
+if [[ $OPT_SHARED == YES ]]; then
     perl -p -i.bak \
-         -e 's#jumbo_component\("pdfium"\) \{#shared_library("pdfium") \{#gi' \
-         $PDFium_SOURCE_DIR/BUILD.gn
+         -e 's#component\("pdfium"\) \{#shared_library("pdfium") \{#gi' \
+         $PDFIUM_SOURCE_DIR/BUILD.gn
 fi
 
-mkdir -p "$PDFium_BUILD_DIR"
+mkdir -p "$PDFIUM_BUILD_DIR"
 # Configure GN args
-cat > "$PDFium_BUILD_DIR/args.gn" <<EOF
+cat > "${PDFIUM_BUILD_DIR}/args.gn" <<EOF
 is_component_build = false
 pdf_enable_v8 = false
 pdf_is_standalone = true
@@ -77,41 +142,20 @@ use_custom_libcxx = false
 # use_sysroot = false
 EOF
 
-if [[ $(getopt -q -l skia -o n -- $@) == *--skia* ]]; then
+if [[ $OPT_SKIA == YES ]]; then
     # use cxx14, for skia
-    echo 'use_cxx11 = false' >> "$PDFium_BUILD_DIR/args.gn"
-    echo 'pdf_use_skia = true' >> "$PDFium_BUILD_DIR/args.gn"
+    echo 'use_cxx11 = false' >> "$PDFIUM_BUILD_DIR/args.gn"
+    echo 'pdf_use_skia = true' >> "$PDFIUM_BUILD_DIR/args.gn"
 fi
 
-if [[ $(getopt -q -l static -o n -- $@) == *--static* ]]; then
-    echo 'pdf_is_complete_lib = true' >> "$PDFium_BUILD_DIR/args.gn"
+if [[ $OPT_STATIC == YES ]]; then
+    echo 'pdf_is_complete_lib = true' >> "$PDFIUM_BUILD_DIR/args.gn"
 fi
 
-[ "$BUILD_MODE" == "RELEASE" ] && echo 'is_debug = false' >> "$PDFium_BUILD_DIR/args.gn"
-[ "$BUILD_MODE" == "DEBUG" ] && echo 'is_debug = true' >> "$PDFium_BUILD_DIR/args.gn"
+[ "$BUILD_MODE" == "RELEASE" ] && echo 'is_debug = false' >> "$PDFIUM_BUILD_DIR/args.gn"
+[ "$BUILD_MODE" == "DEBUG" ] && echo 'is_debug = true' >> "$PDFIUM_BUILD_DIR/args.gn"
 
 
 # Generate Ninja files then build
-gn gen "$PDFium_BUILD_DIR"
-ninja -C "$PDFium_BUILD_DIR" pdfium
-
-# ====== #
-exit 0   #
-# ====== #
-
-# Output
-PDFium_STAGING_DIR="$PDFium_SOURCE_DIR/../lib-pdfium"
-PDFium_INCLUDE_DIR="$PDFium_STAGING_DIR/include"
-PDFium_LIB_DIR="$PDFium_STAGING_DIR/lib"
-mkdir -p "$PDFium_STAGING_DIR"
-mkdir -p "$PDFium_LIB_DIR"
-
-# Install
-cp "$PDFium_SOURCE_DIR/LICENSE" "$PDFium_STAGING_DIR"
-rm -rf "$PDFium_INCLUDE_DIR"
-cp -R "$PDFium_SOURCE_DIR/public" "$PDFium_INCLUDE_DIR"
-rm -f "$PDFium_INCLUDE_DIR/DEPS"
-rm -f "$PDFium_INCLUDE_DIR/README"
-rm -f "$PDFium_INCLUDE_DIR/PRESUBMIT.py"
-[ "$OS" == "linux" ] && cp "$PDFium_BUILD_DIR/libpdfium.so" "$PDFium_LIB_DIR"
-[ "$OS" == "darwin" ] && cp "$PDFium_BUILD_DIR/libpdfium.dylib" "$PDFium_LIB_DIR"
+gn gen "$PDFIUM_BUILD_DIR"
+ninja -C "$PDFIUM_BUILD_DIR" pdfium
