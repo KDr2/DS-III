4053c878e4c69e070ca07771dd1c8cdc7efca0db
KDr2 <zhuo.dev@gmail.com>
2023-08-09 15:09:33 +0800

    update babashka script

--- babashka/klib.clj
+++ babashka/klib.clj
@@ -1,6 +1,18 @@
-(ns klib)
+(ns klib
+  (:require [babashka.fs :as fs]))
+
+(defn home-path [path]
+  (str (System/getenv "HOME") path))
+
+(defn emacs-env []
+  (let [bins [(home-path "/programs/emacs-nox/bin/emacs")
+              (home-path "/programs/emacs.dev/bin/emacs")
+              (home-path "/programs/emacs/bin/emacs")
+              "/usr/bin/emacs"]
+        emacs (first (filter fs/exists? bins))]
+    (if emacs {:EMACS emacs} {})))
 
 (defn show []
   (println "KLib Show!"))
 
-(defn -main [] (show))
+(defn -main [] (println (emacs-env)))
