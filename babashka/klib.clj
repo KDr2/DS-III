afc1d7a078d6900c21ad267fa6a68438a6c6bd6d
KDr2 <zhuo.dev@gmail.com>
2024-11-16 08:22:54 +0800

    delete nix and other unused scripts

--- babashka/klib.clj
+++ babashka/klib.clj
@@ -10,24 +10,17 @@
 (defn home-path [path]
   (str (System/getenv "HOME") path))
 
-(defn nix-shell-cmd [name]
-  (str "nix-shell " script-dir "/../nix/" name "-ns.nix"))
-
-(defn nix-shell [name]
-  (if (= name "list")
-    (doall (->> (.listFiles (io/file (str script-dir "/../nix/")))
-                (map #(.getName %)) (filter #(.endsWith % "-ns.nix"))
-                (map #(.replace % "-ns.nix" "")) (map println)))
-    (shell {:extra-env {:NIXPKGS_ALLOW_UNFREE "1"}}  (nix-shell-cmd name))))
+(defn ds3-path [path]
+  (str (System/getenv "DS3_HOME") path))
 
 (defn emacs-env []
   (let [bins [(home-path "/programs/emacs-nox/bin/emacs")
               (home-path "/programs/emacs.dev/bin/emacs")
               (home-path "/programs/emacs/bin/emacs")
+              (ds3-path "/local/bin/emacs")
               "/usr/bin/emacs"]
-        emacs (first (filter fs/exists? bins))
-        not-in-nix-shell (empty? (System/getenv "IN_NIX_SHELL"))]
-    (if (and not-in-nix-shell emacs) {:EMACS emacs} {})))
+        emacs (first (filter fs/exists? bins))]
+    (if emacs {:EMACS emacs} {})))
 
 (defn show []
   (println "KLib Show!")
--- scripts/tools/keep-if-eth-up.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DEV=${1-eth0}
-
-if [ $(ifconfig $DEV|grep "inet\s"|wc -l) -ge 1 ]; then
-    echo "$DEV is connected."
-else
-    dhclient $DEV
-fi
